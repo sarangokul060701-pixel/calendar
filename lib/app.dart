52f51ac8c9a9d90a63dfce7419ef716d931e4f3e
dhavarsanam <dhavarsanam11@gmail.com>
2026-09-21 17:44:14 +0530

    updated app

---
+++ lib/app.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import '../screens/calendar_screen.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calendar Demo',
+      home: const CalendarScreen(),
+    );
+  }
+}
+import 'package:flutter/material.dart';
+import '../screens/calendar_screen.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calendar Demo',
+      home: const CalendarScreen(),
+    );
+  }
+}
